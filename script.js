f6eec04fbf41a925afe941723a13c6ea1519075c
Swaran Singh <107933244+ADCByDefault@users.noreply.github.com>
2023-09-19 20:22:12 +0200

    "demo working??"

--- script.js
+++ script.js
@@ -1,78 +1,16 @@
-class LineEquation {
-    /**
-     *
-     * @param {number} mx
-     * @param {number} c
-     */
-    constructor(m, c) {
-        this.m = m;
-        this.c = c;
-    }
-    /**
-     * @param {CanvasRenderingContext2D} ctx
-     * @param {HTMLCanvasElement} canvas
-     */
-    draw(ctx, canvas) {
-        ctx.save();
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        ctx.moveTo(0, this.c);
-        ctx.lineTo(canvas.width, this.m * canvas.width + this.c);
-        ctx.stroke();
-        ctx.restore();
-    }
-    /**
-     *
-     * @param {number} x
-     * @returns {number}
-     */
-    getYByX(x) {
-        return this.m * x + this.c;
-    }
-    /**
-     *
-     * @param {Position} pointA
-     * @param {Position} pointB
-     * @returns {LineEquation}
-     */
-    static getEquationByPoints(pointA, pointB) {
-        pointA = { ...pointA };
-        pointB = { ...pointB };
-        if (pointA.x == pointB.x) pointA.x += 0.00000001;
-        let m = (pointB.y - pointA.y) / (pointB.x - pointA.x);
-        let c = pointA.y - m * pointA.x;
-        return new LineEquation(m, c);
-    }
-    /**
-     *
-     * @param {LineEquation} lineB
-     * @param {LineEquation} lineA
-     * @returns {boolean}
-     */
-    static IsParallel(lineA, lineB) {
-        return lineA.m === lineB.m;
-    }
-    /**
-     *
-     * @param {LineEquation} lineA
-     * @param {LineEquation} lineB
-     * @returns {Position}
-     */
-    static getIntersectionPoint(lineA, lineB) {
-        if (LineEquation.IsParallel(lineA, lineB)) return null;
-        let x = (lineB.c - lineA.c) / (lineA.m - lineB.m);
-        let y = lineA.getYByX(x);
-        x = parseInt(x.toFixed(0));
-        y = parseInt(y.toFixed(0));
-        return new Position(x, y);
-    }
-    static getLineIntersectionAngle(lineA, lineB) {
-        if (!lineA || !lineB) return null;
-        let angle = Math.atan((lineA.m - lineB.m) / (1 + lineA.m * lineB.m));
-        return parseInt(((angle * 180) / Math.PI).toFixed(0));
-    }
-}
-class Position {
+/**@type {HTMLCanvasElement} */
+const canvas = document.querySelector("#canvas");
+/**@type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth - 1;
+canvas.height = window.innerHeight - 1;
+const toDraw = new Array();
+const toUpdate = new Array();
+const collisions = new Array();
+
+class Vector {
+    /**@type {CanvasRenderingContext2D} */
+    static ctx = null;
     /**
      *
      * @param {number} x
@@ -82,277 +20,229 @@ class Position {
         this.x = x;
         this.y = y;
     }
-    /**@param {CanvasRenderingContext2D} ctx  */
-    draw(ctx) {
-        ctx.save();
-        ctx.fillStyle = "cyan";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.closePath();
-        ctx.restore();
+    add(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y);
     }
-    /**
-     *
-     * @param {Position} pointA
-     * @param {Position} pointB
-     * @returns
-     */
-    static getDistance(pointA, pointB) {
-        return Math.sqrt(
-            Math.pow(pointB.x - pointA.x, 2) + Math.pow(pointB.y - pointA.y, 2)
-        );
+    subtract(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y);
+    }
+    multiply(scalar) {
+        return new Vector(this.x * scalar, this.y * scalar);
+    }
+    duplicate() {
+        return new Vector(this.x, this.y);
+    }
+    getMagnitude() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+    getUnitVector() {
+        return this.multiply(1 / this.getMagnitude());
+    }
+    dot(vector) {
+        return this.x * vector.x + this.y * vector.y;
+    }
+    draw(dx, dy) {
+        dx = dx ? dx : 0;
+        dy = dy ? dy : 0;
+        /**@type {CanvasRenderingContext2D} */
+        let ctx = Vector.ctx;
+        ctx.beginPath();
+        ctx.moveTo(dx, dy);
+        ctx.lineTo(dx + this.x, dy + this.y);
+        ctx.stroke();
+    }
+    drawAsPoint(dx, dy) {
+        dx = dx ? dx : 0;
+        dy = dy ? dy : 0;
+        /**@type {CanvasRenderingContext2D} */
+        let ctx = Vector.ctx;
+        ctx.beginPath();
+        ctx.arc(dx + this.x, dy + this.y, 2, 0, 2 * Math.PI);
+        ctx.fill();
     }
 }
-class Velocity {
+Vector.ctx = ctx;
+class Collision {
     /**
      *
-     * @param {number} module
-     * @param {number} angle
+     * @param {boolean} collision
+     * @param {Vector} collisionPoint
      */
-    constructor(module, angle) {
-        this.module = module;
-        this.angle = angle % 360;
-        this.angle = this.angle < 0 ? 360 + this.angle : this.angle;
-        this.angle = angle == 90 ? 89.999999999 : this.angle;
-        this.angle = angle == 270 ? 269.999999999 : this.angle;
-    }
-    /**@returns {number} */
-    getSpeedX() {
-        return this.module * Math.cos((this.angle / 180) * Math.PI);
-    }
-    /**@returns {number} */
-    getSpeedY() {
-        return this.module * Math.sin((this.angle / 180) * Math.PI);
-    }
-    draw(ctx, position) {
-        ctx.save();
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        ctx.moveTo(position.x, position.y);
-        ctx.lineTo(
-            position.x + this.getSpeedX(),
-            position.y + this.getSpeedY()
-        );
-        ctx.stroke();
-        ctx.restore();
+    constructor(object1, object2, collision, collisionPoint, relativeVelocity) {
+        this.object1 = object1;
+        this.object2 = object2;
+        this.collision = collision;
+        this.collisionPoint = collisionPoint;
+        this.relativeVelocity = relativeVelocity;
+        this.elasticity = 1;
     }
 }
 class Ball {
-    /**
-     *
-     * @param {Position} position
-     * @param {Velocity} velocity
-     * @param {number} radius
-     */
-    constructor(position, velocity, radius) {
+    static balls = new Array();
+    static acceleration = new Vector(0, 0.1);
+    /**@type {CanvasRenderingContext2D} */
+    static ctx = null;
+    constructor(position, velocity, radius, options) {
         this.position = position;
         this.velocity = velocity;
         this.radius = radius;
+        this.mass = options.mass ? options.mass : 1;
+        this.options = options;
+        toDraw.push(this);
+        toUpdate.push(this);
     }
-    /**@param {CanvasRenderingContext2D} ctx  */
-    draw(ctx) {
+    update() {
+        this.velocity = this.velocity.add(Ball.acceleration);
+        this.position = this.position.add(this.velocity);
+    }
+    draw() {
+        /**
+         * @type {CanvasRenderingContext2D}
+         */
+        let ctx = Ball.ctx;
         ctx.save();
-        ctx.fillStyle = "white";
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = this.options.color;
         ctx.fill();
         ctx.closePath();
         ctx.restore();
     }
-    /**@returns {LineEquation} */
-    getEquation() {
-        let pointA = this.position;
-        let pointB = new Position(
-            this.position.x + this.velocity.getSpeedX(),
-            this.position.y + this.velocity.getSpeedY()
+    //solve collsion by impulse
+    solveCollision(collision) {
+        let elasticity = collision.elasticity;
+        let collisionPoint = collision.collisionPoint;
+        let relativeVelocity = collision.relativeVelocity;
+        let collisionNormal = this.position
+            .subtract(collisionPoint)
+            .getUnitVector();
+        let relativeVelocityAlongNormal = relativeVelocity.dot(collisionNormal);
+        let impulseScalar =
+            (-(1 + elasticity) * relativeVelocityAlongNormal) /
+            (this.getInverseMass() + collision.object2.getInverseMass());
+        let impulse = collisionNormal.multiply(impulseScalar);
+        this.velocity = this.velocity.add(
+            impulse.multiply(this.getInverseMass())
         );
-        return LineEquation.getEquationByPoints(pointA, pointB);
     }
-    /**@param {Position} position  */
-    setPosition(position) {
-        this.position = position;
-    }
-    /**
-     * @param {number} x
-     * @param {number} y
-     */
-    setPosition(x, y) {
-        this.position.x = x;
-        this.position.y = y;
+    getInverseMass() {
+        return 1 / this.mass;
     }
 }
-
-class Box {
-    constructor(position, width, height) {
-        this.position = position;
-        this.width = width;
-        this.height = height;
+Ball.ctx = ctx;
+class Line {
+    static ctx = null;
+    constructor(start, end, options) {
+        this.start = start;
+        this.end = end;
+        this.options = options;
+        this.velocity = new Vector(0, 0);
+        toDraw.push(this);
+        collisions.push(this);
     }
-    /**@param {CanvasRenderingContext2D} ctx  */
-    draw(ctx) {
+    getInverseMass() {
+        let m = this.options.mass ? this.options.mass : Infinity;
+        return 1 / m;
+    }
+    draw() {
+        /**
+         * @type {CanvasRenderingContext2D}
+         */
+        let ctx = Line.ctx;
         ctx.save();
-        ctx.fillStyle = "yellow";
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        ctx.beginPath();
+        ctx.moveTo(this.start.x, this.start.y);
+        ctx.lineTo(this.end.x, this.end.y);
+        ctx.strokeStyle = this.options.color;
+        ctx.lineWidth = this.options.width;
+        ctx.stroke();
+        ctx.closePath();
         ctx.restore();
     }
-    /**@returns {Position[]} */
-    getBoundaries() {
-        return [
-            this.position,
-            new Position(
-                this.position.x + this.width,
-                this.position.y + this.height
-            ),
-        ];
+    getLength() {
+        return this.start.subtract(this.end).getMagnitude();
     }
-    /**@returns {LineEquation[]} */
-    getBoundariesEquations() {
-        let top = LineEquation.getEquationByPoints(
-            ...this.getTopBoundaryPoints()
-        );
-        let right = LineEquation.getEquationByPoints(
-            ...this.getRightBoundaryPoints()
-        );
-        let bottom = LineEquation.getEquationByPoints(
-            ...this.getBottomBoundaryPoints()
-        );
-        let left = LineEquation.getEquationByPoints(
-            ...this.getleftBoundaryPoints()
-        );
-        return [top, right, bottom, left];
+    getNearestPoint(point) {
+        let lineVector = this.end.subtract(this.start);
+        let pointVector = point.subtract(this.start);
+        let lineVectorMagnitude = lineVector.getMagnitude();
+        let lineVectorUnit = lineVector.getUnitVector();
+        let dotProduct =
+            pointVector.x * lineVectorUnit.x + pointVector.y * lineVectorUnit.y;
+        let nearestPointVector = lineVectorUnit.multiply(dotProduct);
+        let nearestPoint = nearestPointVector.add(this.start);
+        if (dotProduct / lineVectorMagnitude < 0) {
+            return this.start;
+        }
+        if (dotProduct / lineVectorMagnitude > 1) {
+            return this.end;
+        }
+        return nearestPoint;
     }
-    /**@returns {Position[]} */
-    getleftBoundaryPoints() {
-        let PointA = this.position;
-        let PointB = new Position(
-            this.position.x,
-            this.position.y + this.height
-        );
-        return [PointA, PointB];
-    }
-    /**@returns {Position[]} */
-    getRightBoundaryPoints() {
-        let PointA = new Position(
-            this.position.x + this.width,
-            this.position.y
-        );
-        let PointB = new Position(
-            this.position.x + this.width,
-            this.position.y + this.height
-        );
-        return [PointA, PointB];
-    }
-    /**@returns {Position[]} */
-    getTopBoundaryPoints() {
-        let PointA = this.position;
-        let PointB = new Position(
-            this.position.x + this.width,
-            this.position.y
-        );
-        return [PointA, PointB];
-    }
-    /**@returns {Position[]} */
-    getBottomBoundaryPoints() {
-        let PointA = new Position(
-            this.position.x,
-            this.position.y + this.height
-        );
-        let PointB = new Position(
-            this.position.x + this.width,
-            this.position.y + this.height
-        );
-        return [PointA, PointB];
-    }
-    /**
-     *
-     * @param {Ball} ball
-     * @returns {Object}
-     */
-    getLinearIntersectionPointWithBall(ball) {
-        let line = ball.getEquation();
-        let intersection = null;
-        let intersectionLine = null;
-        let Distance = null;
-        let boundaries = this.getBoundaries();
-        this.getBoundariesEquations().forEach((eq, i) => {
-            let point = LineEquation.getIntersectionPoint(line, eq);
-            if (point == null) return;
-            {
-                if (point.x < boundaries[0].x || point.x > boundaries[1].x) {
-                    return;
-                }
-                if (point.y < boundaries[0].y || point.y > boundaries[1].y) {
-                    return;
-                }
-            }
-            if (
-                intersection == null ||
-                Position.getDistance(ball.position, point) <
-                    Position.getDistance(ball.position, intersection)
-            ) {
-                intersectionLine = eq;
-                intersection = point;
-                Distance = Position.getDistance(ball.position, point);
-            }
-        });
-        return {
-            intersection: intersection,
-            intersectionLine: intersectionLine,
-            Distance: Distance,
-        };
+    getCollision(ball) {
+        let collision = new Collision(ball, this, false, null, null);
+        collision.relativeVelocity = ball.velocity.subtract(this.velocity);
+        let nearestPoint = this.getNearestPoint(ball.position);
+        let distance = nearestPoint.subtract(ball.position).getMagnitude();
+        collision.collisionPoint = nearestPoint;
+        if (distance < ball.radius) {
+            collision.collision = true;
+        }
+        return collision;
     }
 }
+Line.ctx = ctx;
+const ballOptions = {
+    color: "red",
+    mass: 1,
+};
+const lineOptions = {
+    color: "red",
+    mass: Infinity,
+    width: 1,
+};
+new Line(new Vector(50, 90), new Vector(600, 200), lineOptions);
+new Line(new Vector(600, 200), new Vector(600, 400), lineOptions);
+new Line(new Vector(50, 290), new Vector(600, 400), lineOptions);
+new Line(new Vector(50, 90), new Vector(50, 290), lineOptions);
 
-/**@type {HTMLCanvasElement} */
-const canvas = document.querySelector("#canvas");
-/**@type {CanvasRenderingContext2D} */
-const ctx = canvas.getContext("2d");
+let b = new Ball(new Vector(200, 200), new Vector(0, 0), 30, ballOptions);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const ball = new Ball(new Position(300, 150), new Velocity(10, 150), 5);
-const box = new Box(new Position(200, 200), 100, 100);
-
-window.addEventListener("mousemove", (event) => {});
-
+let req;
 function animate() {
-    // window.requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ball.draw(ctx);
-    box.draw(ctx);
-    let intersection = box.getLinearIntersectionPointWithBall(ball);
-    ball.getEquation().draw(ctx, canvas);
-    intersection.intersectionLine?.draw(ctx, canvas);
-    let neweq;
-    if (ball.position.y < intersection.intersection.y) {
-        neweq = new Velocity(
-            ball.velocity.module + 100,
-            -LineEquation.getLineIntersectionAngle(
-                ball.getEquation(),
-                intersection.intersectionLine
-            ) + 180
-        );
-        console.log("ciao");
-        neweq.draw(ctx, intersection.intersection);
-    }
-    let v = new Velocity(
-        ball.velocity.module,
-        -LineEquation.getLineIntersectionAngle(
-            ball.getEquation(),
-            intersection.intersectionLine
-        )
-    );
-    ball.position.x += ball.velocity.getSpeedX();
-    ball.position.y += ball.velocity.getSpeedY();
-    if (
-        Position.getDistance(ball.position, intersection.intersection) <
-        ball.radius
-    ) {
-        ball.velocity = v;
-    }
-    v.draw(ctx, intersection.intersection);
+    toDraw.forEach((e) => {
+        e.draw();
+    });
+    collisions.forEach((e) => {
+        let collision = e.getCollision(b);
+        drawPoint(collision.collisionPoint);
+        drawLine(b.position, collision.collisionPoint);
+        if (collision.collision) {
+            b.solveCollision(collision);
+        }
+        b.options.color = collision.collision ? "green" : "red";
+    });
+    toUpdate.forEach((e) => {
+        e.update();
+    });
+    req = window.requestAnimationFrame(animate);
+}
+req = window.requestAnimationFrame(animate);
+
+window.addEventListener("mousemove", (e) => {
+    b.position = new Vector(e.clientX, e.clientY);
+});
+
+function drawLine(vec1, vec2) {
+    ctx.beginPath();
+    ctx.moveTo(vec1.x, vec1.y);
+    ctx.lineTo(vec2.x, vec2.y);
+    ctx.stroke();
 }
 
-animate();
+function drawPoint(vec) {
+    ctx.beginPath();
+    ctx.arc(vec.x, vec.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
